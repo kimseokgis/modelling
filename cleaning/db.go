1bd6b218490591d398aa2b4562956b0f38720eb3
rofinafiin <rofinafiisr@gmail.com>
2024-07-07 20:35:39 +0700

    Preprocessing - Go : set per page 150 data for handling timeout

--- cleaning/db.go
+++ cleaning/db.go
@@ -25,21 +25,29 @@ func DBAdmn(dbname string) *mongo.Database {
 }
 
 // Fungsi untuk mengambil semua dokumen dari koleksi "dataset"
-func GetAllDatasets(ctx context.Context) ([]Dataset, error) {
-	db := DBAdmn("AI")
+func GetAllDatasets(ctx context.Context, params PaginationParams, db *mongo.Database) ([]Dataset, error) {
+	if params.Page < 1 {
+		return nil, fmt.Errorf("Page number must be greater than 0")
+	}
+	if params.Limit < 1 {
+		return nil, fmt.Errorf("Page size must be greater than 0")
+	}
+
 	collection := db.Collection("datasets")
+	filter := bson.M{}
 
-	// Membuat filter kosong untuk mengambil semua dokumen
-	filter := bson.D{}
+	// Calculate the number of documents to skip
 
-	// Melakukan query untuk mengambil semua dokumen
-	cur, err := collection.Find(ctx, filter)
+	findOptions := options.Find()
+	findOptions.SetLimit(params.Limit)
+	findOptions.SetSkip(params.Page*params.Limit - params.Limit)
+
+	cur, err := collection.Find(ctx, filter, findOptions)
 	if err != nil {
 		return nil, fmt.Errorf("Find: %+v \n", err)
 	}
 	defer cur.Close(context.TODO())
 
-	// Mengambil hasil query dan memasukkannya ke slice Dataset
 	var datasets []Dataset
 	for cur.Next(context.TODO()) {
 		var dataset Dataset
@@ -55,11 +63,12 @@ func GetAllDatasets(ctx context.Context) ([]Dataset, error) {
 		return nil, fmt.Errorf("Cursor: %+v \n", err)
 	}
 
+	defer cur.Close(context.TODO())
+	//defer db.Client().Disconnect(context.TODO())
 	return datasets, nil
 }
 
-func GetDatasetByID(id primitive.ObjectID) (*Dataset, error) {
-	db := DBAdmn("AI")
+func GetDatasetByID(id primitive.ObjectID, db *mongo.Database) (*Dataset, error) {
 	collection := db.Collection("datasets")
 
 	// Membuat filter berdasarkan _id
@@ -74,30 +83,23 @@ func GetDatasetByID(id primitive.ObjectID) (*Dataset, error) {
 		}
 		return nil, fmt.Errorf("FindOne: %+v", err)
 	}
-
+	//defer db.Client().Disconnect(context.TODO())
 	return &dataset, nil
 }
 
-func UpdateDatasetByID(id primitive.ObjectID, updateData Dataset) error {
-	db := DBAdmn("AI")
+func UpdateDatasetByID(id primitive.ObjectID, updateData Dataset, db *mongo.Database) (err error) {
 	collection := db.Collection("datasets")
-
 	// Membuat filter berdasarkan _id
-	filter := bson.D{{"_id", id}}
-
-	// Membuat opsi untuk mengatur opsi dokumen yang diperbarui
-	opts := options.Update().SetUpsert(false)
-
+	filter := bson.M{"_id": id}
 	// Membuat operasi update
-	update := bson.D{
-		{"$set", updateData},
+	update := bson.M{
+		"$set": updateData,
 	}
-
 	// Melakukan update dokumen
-	_, err := collection.UpdateOne(context.TODO(), filter, update, opts)
+	err = collection.FindOneAndUpdate(context.TODO(), filter, update).Err()
 	if err != nil {
 		return fmt.Errorf("UpdateOne: %+v", err)
 	}
-
+	//defer db.Client().Disconnect(context.TODO())
 	return nil
 }
