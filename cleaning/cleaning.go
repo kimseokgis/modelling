789164573b4644670b2ee61e656d38b7cec4f497
rofinafiin <rofinafiisr@gmail.com>
2024-07-07 20:35:17 +0700

    Preprocessing - Go : set defer close after update data

--- cleaning/cleaning.go
+++ cleaning/cleaning.go
@@ -2,70 +2,84 @@ package cleaning
 
 import (
 	"context"
+	"fmt"
 	"strings"
-	"time"
 )
 
 func Cleaningdata() (Err error) {
 	// Membuat konteks dengan timeout
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	data, err := GetAllDatasets(ctx)
-	if err != nil {
-		return err
-	}
-
-	for _, v := range data {
-		symbols := []string{
-			"!",
-			"@",
-			"#",
-			"$",
-			"%",
-			"^",
-			"&",
-			"*",
-			"(",
-			")",
-			"-",
-			"+",
-			"=",
-			"[",
-			"]",
-			"{",
-			"}",
-			";",
-			":",
-			"'",
-			//"\"",
-			//"\\",
-			"|",
-			",",
-			".",
-			"<",
-			">",
-			"/",
-			"?",
-			"\n",
-			",",
+	//ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	//defer cancel()
+	db := DBAdmn("AI")
+	var datas []Dataset
+	re := 100
+	for i := 1; i < re; i++ {
+		params := PaginationParams{
+			Page:  int64(i),
+			Limit: 150,
+			//Offset: 0,
 		}
-
-		for _, symbol := range symbols {
-			if strings.ContainsAny(v.Answers, symbol) {
-				v.Answers = strings.Replace(v.Answers, symbol, "", 10)
-			}
-		}
-
-		valueUpdated := Dataset{
-			//ID:        primitive.ObjectID{},
-			Questions: v.Questions,
-			Answers:   v.Answers,
-		}
-
-		err = UpdateDatasetByID(v.ID, valueUpdated)
+		data, err := GetAllDatasets(context.TODO(), params, db)
 		if err != nil {
 			return err
 		}
+		datas = data
+
+		fmt.Printf("%d\n", i)
+
+		fmt.Println(datas)
+
+		for _, v := range datas {
+			for _, symbol := range symbols {
+				if strings.ContainsAny(v.Answers, symbol) {
+					v.Answers = strings.Replace(v.Answers, symbol, "", 10)
+				}
+			}
+			valueUpdated := Dataset{
+				Questions: v.Questions,
+				Answers:   v.Answers,
+			}
+
+			err := UpdateDatasetByID(v.ID, valueUpdated, db)
+			if err != nil {
+				return err
+			}
+		}
+		defer db.Client().Disconnect(context.TODO())
 	}
-	return err
+	return
+}
+
+var symbols = []string{
+	"!",
+	"@",
+	"#",
+	"$",
+	"%",
+	"^",
+	"&",
+	"*",
+	"(",
+	")",
+	"-",
+	"+",
+	"=",
+	"[",
+	"]",
+	"{",
+	"}",
+	";",
+	":",
+	"'",
+	//"\"",
+	//"\\",
+	"|",
+	",",
+	".",
+	"<",
+	">",
+	"/",
+	"?",
+	"\n",
+	",",
 }
