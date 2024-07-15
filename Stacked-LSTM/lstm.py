c3d077bd8a6b19b22cf18658aaaec8d7c67b6d4c
Daffa Audya Pramana <daffaaudyapramana03@gmail.com>
2024-07-15 20:59:58 +0700

    Preprocessing - py : Updating Stacked LSTM Model

--- Stacked-LSTM/lstm.py
+++ Stacked-LSTM/lstm.py
@@ -1,8 +1,6 @@
-# -*- coding: utf-8 -*-
 import json
 import os
 import pickle
-
 import pandas as pd
 import tensorflow as tf
 from keras import Input, Model
@@ -17,10 +15,7 @@ from keras_preprocessing.text import Tokenizer
 sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(log_device_placement=True))
 
 path = "output_dir/"
-try:
-    os.makedirs(path)
-except:
-    pass
+os.makedirs(path, exist_ok=True)
 
 dataset = pd.read_csv('qa.csv', delimiter="|", header=None, lineterminator='\n')
 
@@ -35,11 +30,9 @@ answers_train = dataset.iloc[:1794, 1].values.tolist()
 questions_test = dataset.iloc[1794:, 0].values.tolist()
 answers_test = dataset.iloc[1794:, 1].values.tolist()
 
-
 def save_tokenizer(tokenizer):
     with open('output_dir/tokenizer.pickle', 'wb') as handle:
         pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
 
 def save_config(key, value):
     data = {}
@@ -68,22 +61,22 @@ save_config('VOCAB_SIZE', VOCAB_SIZE)
 print('Vocabulary size : {}'.format(VOCAB_SIZE))
 
 tokenized_questions_train = tokenizer.texts_to_sequences(questions_train)
-maxlen_questions_train = max([len(x) for x in tokenized_questions_train])
+maxlen_questions_train = max([len(x) for x in tokenized_questions_train]) if tokenized_questions_train else 0
 save_config('maxlen_questions', maxlen_questions_train)
 encoder_input_data_train = pad_sequences(tokenized_questions_train, maxlen=maxlen_questions_train, padding='post')
 
 tokenized_questions_test = tokenizer.texts_to_sequences(questions_test)
-maxlen_questions_test = max([len(x) for x in tokenized_questions_test])
+maxlen_questions_test = max([len(x) for x in tokenized_questions_test]) if tokenized_questions_test else 0
 save_config('maxlen_questions', maxlen_questions_test)
 encoder_input_data_test = pad_sequences(tokenized_questions_test, maxlen=maxlen_questions_test, padding='post')
 
 tokenized_answers_train = tokenizer.texts_to_sequences(answers_train)
-maxlen_answers_train = max([len(x) for x in tokenized_answers_train])
+maxlen_answers_train = max([len(x) for x in tokenized_answers_train]) if tokenized_answers_train else 0
 save_config('maxlen_answers', maxlen_answers_train)
 decoder_input_data_train = pad_sequences(tokenized_answers_train, maxlen=maxlen_answers_train, padding='post')
 
 tokenized_answers_test = tokenizer.texts_to_sequences(answers_test)
-maxlen_answers_test = max([len(x) for x in tokenized_answers_test])
+maxlen_answers_test = max([len(x) for x in tokenized_answers_test]) if tokenized_answers_test else 0
 save_config('maxlen_answers', maxlen_answers_test)
 decoder_input_data_test = pad_sequences(tokenized_answers_test, maxlen=maxlen_answers_test, padding='post')
 
