d3a4f791c57dac559d58d5cf1795d6c24b16081a
Daffa Audya P <daffaaudyapramana03@gmail.com>
2024-07-22 23:54:42 +0700

    update bert.py

--- dapa/modelbert.py
+++ dapa/modelbert.py
@@ -3,3 +3,10 @@ from transformers import TFBertForSequenceClassification, BertTokenizer
 import pandas as pd
 import csv
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+
+# Load the dataset
+def filter_valid_rows(row):
+    return len(row) == 2
+
+with open('../Stacked-LSTM/qa.csv', 'r', encoding='utf-8') as file:
