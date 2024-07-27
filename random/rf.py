1c4b3ea3f0843e31c4984f4cb245c13ab3f137e4
harisriyoni3 <1214047@std.ulbi.ac.id>
2024-07-28 00:01:04 +0700

    fungsi eval model

--- random/rf.py
+++ random/rf.py
@@ -71,6 +71,11 @@ def train_random_forest(X_train, y_train, n_estimators, random_state):
     rf_classifier.fit(X_train, y_train)
     return rf_classifier
 
+def evaluate_model(model, X_test, y_test, label_encoder):
+    """
+    Evaluate the model and print the accuracy and classification report.
+    """
+
 X_train, X_test, y_train, y_test = train_test_split(combined_text, labels, test_size=0.2, random_state=42)
 
 tfidf_vectorizer = TfidfVectorizer(max_features=1000)
