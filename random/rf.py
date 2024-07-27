65d6cd4b000f011ac2453a1aecc4bbe93e29fc9d
harisriyoni3 <1214047@std.ulbi.ac.id>
2024-07-28 00:06:01 +0700

    set parameter 1

--- random/rf.py
+++ random/rf.py
@@ -95,6 +95,13 @@ def save_model(model, vectorizer, label_encoder, path):
     with open(os.path.join(path, 'label_encoder.pkl'), 'wb') as encoder_file:
         pickle.dump(label_encoder, encoder_file)
 
+def main():
+    # Set parameters
+    path = "output_dir_14k/"
+    file_path = 'data.csv'
+    delimiter = "|"
+    header = None   
+
 
 X_train, X_test, y_train, y_test = train_test_split(combined_text, labels, test_size=0.2, random_state=42)
 
