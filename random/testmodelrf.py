f482ab8f3595f37cfe17ddf3d4dcc23d28a9fcf9
harisriyoni3 <1214047@std.ulbi.ac.id>
2024-07-28 00:21:27 +0700

    import yang dibutuhkan

---
+++ random/testmodelrf.py
@@ -0,0 +1,8 @@
+import os
+import pickle
+import logging
+import pandas as pd
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
