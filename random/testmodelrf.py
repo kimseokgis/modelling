f7994747e83ebc809aed5d69cdb20350843d979e
harisriyoni3 <1214047@std.ulbi.ac.id>
2024-07-28 00:21:46 +0700

    configure loogingnya

--- random/testmodelrf.py
+++ random/testmodelrf.py
@@ -6,3 +6,7 @@ from sklearn.metrics import accuracy_score, classification_report, confusion_mat
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder
+
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
