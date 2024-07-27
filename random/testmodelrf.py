fbeaff2debbcf798df9d657d7d0be2a074a76239
harisriyoni3 <1214047@std.ulbi.ac.id>
2024-07-28 00:23:03 +0700

    load test

--- random/testmodelrf.py
+++ random/testmodelrf.py
@@ -18,3 +18,8 @@ def load_pickle(file_path):
     """
     with open(file_path, 'rb') as file:
         return pickle.load(file)
+    
+def load_test_dataset(file_path, delimiter, header, lineterminator):
+    """
+    Load the test dataset from a CSV file.
+    """
