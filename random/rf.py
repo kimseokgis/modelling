741f7f280e9a4aef6f5f3360c4a15f80f328dc58
harisriyoni3 <1214047@std.ulbi.ac.id>
2024-07-27 20:55:19 +0700

    Vectorize text data using TF-IDF

--- random/rf.py
+++ random/rf.py
@@ -24,3 +24,7 @@ labels = label_encoder.fit_transform(answers)
 # Split the data into training and testing sets
 
 X_train, X_test, y_train, y_test = train_test_split(combined_text, labels, test_size=0.2, random_state=42)
+
+tfidf_vectorizer = TfidfVectorizer(max_features=1000)
+X_train_tfidf = tfidf_vectorizer.fit_transform(X_train)
+X_test_tfidf = tfidf_vectorizer.transform(X_test)
