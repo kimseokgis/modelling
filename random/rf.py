cdb522f9337dfc01790f4b7366745e4ffe213c36
harisriyoni3 <1214047@std.ulbi.ac.id>
2024-07-27 20:49:01 +0700

    import file yang diperlukan

---
+++ random/rf.py
@@ -0,0 +1,7 @@
+import os
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
