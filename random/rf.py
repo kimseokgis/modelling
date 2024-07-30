1ef024f3e680c3a57d9678a2810b6b64ee5b22cc
Farhan Rizki <farhanrizki101010@gmail.com>
2024-07-31 01:55:34 +0700

    delete random rf

--- random/rf.py
+++ random/rf.py
@@ -1,9 +1,7 @@
 def main():
     
     
-    test_size = 0.2
-            random_state = 42
-    max_features = 1000
+    
     n_estimators = 100
         # Create output directory
     create_output_directory(path)
