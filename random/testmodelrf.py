65b4d7ca97652e494ad09826a3c0b860c4459ee3
Farhan Rizki <farhanrizki101010@gmail.com>
2024-07-31 02:02:20 +0700

    delete logging

--- random/testmodelrf.py
+++ random/testmodelrf.py
@@ -1,7 +1,6 @@
 def evaluate_model(model, X_test, y_test, label_encoder):
     
-    logging.info("Evaluating model on test data")
-    y_pred = model.predict(X_test)
+    
     
     accuracy = accuracy_score(y_test, y_pred)
             report = classification_report(y_test, y_pred, target_names=label_encoder.classes_)
