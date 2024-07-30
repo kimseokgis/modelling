96c97d6176f848c738645eed7c4fa81a8dd8bfe4
Farhan Rizki <farhanrizki101010@gmail.com>
2024-07-31 01:58:33 +0700

    delete

--- random/rf.py
+++ random/rf.py
@@ -20,8 +20,7 @@ def main():
 
     
 
-    # Save the model, vectorizer, and label encoder
-    save_model(rf_classifier, tfidf_vectorizer, label_encoder, path)
+    
 
 if __name__ == "__main__":
     main()
