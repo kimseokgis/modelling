6f96bde1c07881bf635809e06d4ad2ecfcc6e639
Farhan Rizki <farhanrizki101010@gmail.com>
2024-07-31 02:10:04 +0700

    delete model

--- bert/model.py
+++ bert/model.py
@@ -3,9 +3,7 @@
 
 
 
-with open('../Stacked-LSTM/qa.csv', 'r', encoding='utf-8') as file:
-    reader = csv.reader(file, delimiter='|')
-    filtered_rows = [row for row in reader if filter_valid_rows(row)]
+
 
 df = pd.DataFrame(filtered_rows, columns=['question', 'answer'])
 
