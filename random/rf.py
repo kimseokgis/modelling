d27f9da88cb3a3f1d16082c4cecb2729cf407a16
Daffa Audya P <daffaaudyapramana03@gmail.com>
2024-07-31 00:25:10 +0700

    delete import

--- random/rf.py
+++ random/rf.py
@@ -1,5 +1,3 @@
-from sklearn.metrics import accuracy_score, classification_report
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
 # Configure logging
