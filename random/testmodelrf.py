dcc936680f61f01b4f0b49b72790a2146f319b2d
Farhan Rizki <farhanrizki101010@gmail.com>
2024-07-31 02:06:20 +0700

    delete save

--- random/testmodelrf.py
+++ random/testmodelrf.py
@@ -3,8 +3,7 @@ def evaluate_model(model, X_test, y_test, label_encoder):
 def main():
     
     # Save evaluation results
-    evaluation_path = "output_dir_14k/evaluation_results.txt"
-    with open(evaluation_path, 'w') as eval_file:
+    
         eval_file.write(f"Accuracy: {accuracy}\n")
         eval_file.write("Classification Report:\n")
         eval_file.write(report + "\n")
