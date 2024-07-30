122e009b847fd5c6df6bbbbe6b7e1548e82a5ae9
Farhan Rizki <farhanrizki101010@gmail.com>
2024-07-31 01:58:17 +0700

    delete evaluate

--- random/rf.py
+++ random/rf.py
@@ -18,8 +18,7 @@ def main():
 
     
 
-    # Evaluate the model
-    evaluate_model(rf_classifier, X_test_tfidf, y_test, label_encoder)
+    
 
     # Save the model, vectorizer, and label encoder
     save_model(rf_classifier, tfidf_vectorizer, label_encoder, path)
