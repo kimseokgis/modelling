5713adb0f780d140959a9295cabd4a97ebf96ed9
Farhan Rizki <farhanrizki101010@gmail.com>
2024-07-31 02:20:07 +0700

    add load

--- bert/model.py
+++ bert/model.py
@@ -6,3 +6,7 @@ import pandas as pd
 import csv
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
+
+# Load the dataset
+def filter_valid_rows(row):
+    return len(row) == 2
