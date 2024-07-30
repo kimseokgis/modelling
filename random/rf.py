0008b389791254d6d9d5403119a95a7f4653a437
Daffa Audya P <daffaaudyapramana03@gmail.com>
2024-07-31 00:56:44 +0700

    logging training w randomforestclassifier

--- random/rf.py
+++ random/rf.py
@@ -1,5 +1,3 @@
-    """
-    logging.info(f"Training RandomForestClassifier with {n_estimators} estimators")
     rf_classifier = RandomForestClassifier(n_estimators=n_estimators, random_state=random_state)
     rf_classifier.fit(X_train, y_train)
     return rf_classifier
