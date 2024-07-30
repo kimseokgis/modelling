56302ed2e95ae0377aef737524529b468d1fbf0f
Farhan Rizki <farhanrizki101010@gmail.com>
2024-07-31 02:06:39 +0700

    delete evaluation

--- random/testmodelrf.py
+++ random/testmodelrf.py
@@ -4,10 +4,7 @@ def main():
     
     # Save evaluation results
     
-        eval_file.write(f"Accuracy: {accuracy}\n")
-        eval_file.write("Classification Report:\n")
-        eval_file.write(report + "\n")
-        eval_file.write(f"Confusion Matrix:\n{conf_matrix}\n")
+        
     
     logging.info(f"Evaluation results saved to: {evaluation_path}")
 
