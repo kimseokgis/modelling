8e79654b23dbc03334001ef936650ded3ffbba8e
Farhan Rizki <farhanrizki101010@gmail.com>
2024-07-31 02:11:36 +0700

    delete

--- bert/model.py
+++ bert/model.py
@@ -1,7 +1,5 @@
 
-# Encode the labels (answers) to numeric values
-label_encoder = LabelEncoder()
-df['encoded_answer'] = label_encoder.fit_transform(df['answer'])
+
 
 # Prepare the dataset
 tokenizer = BertTokenizer.from_pretrained('indobenchmark/indobert-base-p2')
