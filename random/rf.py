143835c525aa2b9305af204fa282f9e3944d96cc
Daffa Audya P <daffaaudyapramana03@gmail.com>
2024-07-31 00:53:19 +0700

    delete x train test

--- random/rf.py
+++ random/rf.py
@@ -1,5 +1,3 @@
-    X_train_tfidf = tfidf_vectorizer.fit_transform(text_train)
-    X_test_tfidf = tfidf_vectorizer.transform(text_test)
     return X_train_tfidf, X_test_tfidf, tfidf_vectorizer
 
 def train_random_forest(X_train, y_train, n_estimators, random_state):
