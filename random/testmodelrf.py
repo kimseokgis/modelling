d7374568018fdd6abaf452ebf4f2e3df8f2ed1cc
Farhan Rizki <farhanrizki101010@gmail.com>
2024-07-31 02:06:54 +0700

    delete

--- random/testmodelrf.py
+++ random/testmodelrf.py
@@ -1,12 +1,6 @@
 def evaluate_model(model, X_test, y_test, label_encoder):
 
 def main():
-    
-    # Save evaluation results
-    
-        
-    
-    logging.info(f"Evaluation results saved to: {evaluation_path}")
 
 if __name__ == "__main__":
     main()
