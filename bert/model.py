4c280ed7b87fa9f6085ed25833ff3d799411c6f3
Farhan Rizki <farhanrizki101010@gmail.com>
2024-07-31 02:09:02 +0700

    delete import

--- bert/model.py
+++ bert/model.py
@@ -1,7 +1,4 @@
-import tensorflow as tf
-from transformers import TFBertForSequenceClassification, BertTokenizer
-import pandas as pd
-import csv
+
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
