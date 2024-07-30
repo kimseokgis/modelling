01203bde79839d7d61b8592a75951f75223f8973
Farhan Rizki <farhanrizki101010@gmail.com>
2024-07-31 02:19:41 +0700

    add from

--- bert/model.py
+++ bert/model.py
@@ -4,3 +4,5 @@ import tensorflow as tf
 from transformers import TFBertForSequenceClassification, BertTokenizer
 import pandas as pd
 import csv
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
