ab98f108da36fb1864c284ac86f484fd465be0d6
Daffa Audya P <daffaaudyapramana03@gmail.com>
2024-07-31 00:24:54 +0700

    delete import

--- random/rf.py
+++ random/rf.py
@@ -1,5 +1,3 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
