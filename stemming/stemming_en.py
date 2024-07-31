f1bdec0451928dfe8a6c9e0dc047ae60360b1fed
rofinafiin <rofinafiisr@gmail.com>
2024-07-31 21:35:10 +0700

    Preprocessing - py : mendefinisikan fungsi untuk stemming kalimat bahasa inggris

--- stemming/stemming_en.py
+++ stemming/stemming_en.py
@@ -2,3 +2,14 @@ import pandas as pd
 import nltk
 from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
+
+# Pastikan untuk mengunduh data yang diperlukan
+nltk.download('punkt')
+
+# Inisialisasi PorterStemmer
+porter = PorterStemmer()
+
+# Fungsi untuk stemming menggunakan PorterStemmer
+def stem_sentence(sentence):
+    words = word_tokenize(sentence)
+    return " ".join([porter.stem(word) for word in words])
