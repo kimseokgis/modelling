23001bb1e3c09751c14ca805dcdf04b85386be78
rofinafiin <rofinafiisr@gmail.com>
2024-07-31 21:38:24 +0700

    Preprocessing - py : mendefinisikan fungsi stem sentence

--- stemming/stemming_id.py
+++ stemming/stemming_id.py
@@ -1,3 +1,12 @@
 import pandas as pd
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 from nltk.tokenize import word_tokenize
+
+# Inisialisasi stemmer bahasa Indonesia
+factory = StemmerFactory()
+stemmer = factory.create_stemmer()
+
+# Fungsi untuk stemming menggunakan Sastrawi
+def stem_sentence(sentence):
+    words = word_tokenize(sentence)
+    return " ".join([stemmer.stem(word) for word in words])
