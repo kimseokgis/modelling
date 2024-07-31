3f2bd7f49c50912107ff8d77cb5e178e38b190ac
rofinafiin <rofinafiisr@gmail.com>
2024-07-31 21:03:49 +0700

    Preprocessing - py : loading dataframe

--- test.py
+++ test.py
@@ -8,3 +8,12 @@ import csv
 tokenizer = BertTokenizer.from_pretrained('indobert_model')
 model = TFBertForSequenceClassification.from_pretrained('indobert_model')
 
+
+# Load the label encoder
+with open('Stacked-LSTM/qa.csv', 'r', encoding='utf-8') as file:
+    reader = csv.reader(file, delimiter='|')
+    filtered_rows = [row for row in reader if len(row) == 2]
+
+df = pd.DataFrame(filtered_rows, columns=['question', 'answer'])
+label_encoder = LabelEncoder()
+label_encoder.fit(df['answer'])
