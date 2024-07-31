dc87064936ad09fa8cd60e6225793c73d4bfe331
rofinafiin <rofinafiisr@gmail.com>
2024-07-31 22:12:16 +0700

    Cleaning - go : comment unused test

--- cleaning/cleaning_test.go
+++ cleaning/cleaning_test.go
@@ -1,24 +1,18 @@
 package cleaning
 
-import (
-	"fmt"
-	"strings"
-	"testing"
-)
-
-func TestGetAllDatasets(t *testing.T) {
-	data, err := GetAllDatasets()
-
-	dat, err := GetDatasetByID(data[0].ID)
-
-	upt := Dataset{
-		//ID:        dat.ID,
-		Questions: dat.Questions,
-		Answers:   strings.Replace(dat.Answers, "?", "", 1),
-	}
-
-	err = UpdateDatasetByID(dat.ID, upt)
-
-	fmt.Printf("Data: %+v\n", dat)
-	fmt.Printf("Error: %+v\n", err)
-}
+//func TestGetAllDatasets(t *testing.T) {
+//	data, err := GetAllDatasets()
+//
+//	dat, err := GetDatasetByID(data[0].ID)
+//
+//	upt := Dataset{
+//		//ID:        dat.ID,
+//		Questions: dat.Questions,
+//		Answers:   strings.Replace(dat.Answers, "?", "", 1),
+//	}
+//
+//	err = UpdateDatasetByID(dat.ID, upt)
+//
+//	fmt.Printf("Data: %+v\n", dat)
+//	fmt.Printf("Error: %+v\n", err)
+//}
