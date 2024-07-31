6e81d17e2075b5fb5ab87d611d5700558144c17f
rofinafiin <rofinafiisr@gmail.com>
2024-07-31 21:04:13 +0700

    Preprocessing - py : definition predict functions

--- test.py
+++ test.py
@@ -17,3 +17,26 @@ with open('Stacked-LSTM/qa.csv', 'r', encoding='utf-8') as file:
 df = pd.DataFrame(filtered_rows, columns=['question', 'answer'])
 label_encoder = LabelEncoder()
 label_encoder.fit(df['answer'])
+
+
+# Function to make predictions
+def predict(question):
+    encoded = tokenizer.encode_plus(
+        question,
+        add_special_tokens=True,
+        max_length=64,
+        padding='max_length',
+        truncation=True,
+        return_attention_mask=True,
+        return_tensors='tf'
+    )
+
+    input_ids = encoded['input_ids']
+    attention_mask = encoded['attention_mask']
+
+    logits = model(input_ids, attention_mask=attention_mask).logits
+    predicted_label_id = tf.argmax(logits, axis=1).numpy()[0]
+    predicted_label = label_encoder.inverse_transform([predicted_label_id])[0]
+
+    return predicted_label
+
