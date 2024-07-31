7665690e3c5a3b38e53b8144a7ecf3153688729f
rofinafiin <rofinafiisr@gmail.com>
2024-07-31 21:35:45 +0700

    Preprocessing - py : stemming csv

--- stemming/stemming_en.py
+++ stemming/stemming_en.py
@@ -13,3 +13,15 @@ porter = PorterStemmer()
 def stem_sentence(sentence):
     words = word_tokenize(sentence)
     return " ".join([porter.stem(word) for word in words])
+
+# Baca file CSV ke dalam DataFrame
+df = pd.read_csv('dataset/dataset.csv')
+
+# Asumsikan kolom kalimat bernama 'sentence'
+df['stemmed_sentence'] = df['sentence'].apply(stem_sentence)
+
+# Tampilkan DataFrame dengan kalimat yang sudah di-stem
+print(df.head())
+
+# Simpan DataFrame hasil stemming ke file CSV baru
+df.to_csv('dataset/stemmed_dataset.csv', index=False)
