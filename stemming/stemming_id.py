2668208077c6e3e131d86c30feee2e9e6058f7af
rofinafiin <rofinafiisr@gmail.com>
2024-07-31 21:39:04 +0700

    Preprocessing - py : melakukan stemming kata indo

--- stemming/stemming_id.py
+++ stemming/stemming_id.py
@@ -10,3 +10,15 @@ stemmer = factory.create_stemmer()
 def stem_sentence(sentence):
     words = word_tokenize(sentence)
     return " ".join([stemmer.stem(word) for word in words])
+
+# Baca file CSV ke dalam DataFrame
+df = pd.read_csv('dataset/dataset.csv')
+
+# Asumsikan kolom kalimat bernama 'sentence'
+df['stemmed_sentence'] = df['question'].apply(stem_sentence)
+
+# Tampilkan DataFrame dengan kalimat yang sudah di-stem
+print(df.head())
+
+# Simpan DataFrame hasil stemming ke file CSV baru
+df.to_csv('dataset/id/stemmed_dataset.csv', index=False)
