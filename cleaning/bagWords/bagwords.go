9e4460b3b6ce50a534054c9096a4bde505f0a08f
rofinafiin <rofinafiisr@gmail.com>
2024-07-31 22:04:10 +0700

    Cleaning - go : mendefinisikan fungsi untuk bagofwords

--- cleaning/bagWords/bagwords.go
+++ cleaning/bagWords/bagwords.go
@@ -1,1 +1,18 @@
 package bagWords
+
+import "strings"
+
+func BagOfWords(text string) map[string]int {
+	// Membuat map untuk menyimpan frekuensi kata
+	wordFreq := make(map[string]int)
+
+	// Memisahkan teks menjadi kata-kata
+	words := strings.Fields(text)
+
+	// Menghitung frekuensi setiap kata
+	for _, word := range words {
+		wordFreq[strings.ToLower(word)]++ // Menggunakan toLower untuk konsistensi
+	}
+
+	return wordFreq
+}
