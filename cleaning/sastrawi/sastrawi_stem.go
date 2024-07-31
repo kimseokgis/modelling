7c191da65461b80719f402eb707c2e46f3996cf7
rofinafiin <rofinafiisr@gmail.com>
2024-07-31 22:07:29 +0700

    Cleaning - go : define stemmer function

--- cleaning/sastrawi/sastrawi_stem.go
+++ cleaning/sastrawi/sastrawi_stem.go
@@ -1,1 +1,17 @@
 package sastrawi
+
+import (
+	"github.com/RadhiFadlillah/go-sastrawi"
+	"strings"
+)
+
+func Stemmer(Sentences string) (newString string) {
+	dictionary := sastrawi.DefaultDictionary()
+	stemmer := sastrawi.NewStemmer(dictionary)
+	for _, word := range sastrawi.Tokenize(Sentences) {
+		//fmt.Println(word)
+		newString = newString + " " + stemmer.Stem(word)
+		//fmt.Println(newString)
+	}
+	return strings.TrimSpace(newString)
+}
